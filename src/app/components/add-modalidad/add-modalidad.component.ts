707418ac50d4f0ffb3aa16dbe23fb7c2892d00b6
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-09 20:45:06 -0500

    front end angular 18 - inicio

---
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-modalidad',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-modalidad.component.html',
+  styleUrl: './add-modalidad.component.css'
+})
+export class AddModalidadComponent {
+
+}
