e5d273f57bc5504978793f86c3ba16fc58227b25
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-10 13:02:58 -0500

    deporte service terminado

--- src/app/services/deporte.service.ts
+++ src/app/services/deporte.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Deporte } from '../model/deporte';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeporteService {
 
-  constructor() { }
+  private url = "https://localhost:8090/url/util";
+
+  constructor(private http : HttpClient) { }
+
+  listaDeporte() : Observable<Deporte[]> {
+    return this.http.get<Deporte[]>(this.url + "/deporte");
+  }
 }
